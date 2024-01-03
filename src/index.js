031d56e2de67d0d0ac14129b15e422a26349bae6
paul steane <psteane123@hotmail.comnpm install @mui/material @emotion/react @emotion/styled>
2024-01-03 10:32:34 +0000

    dynamic list

--- src/index.js
+++ src/index.js
@@ -8,16 +8,25 @@ const firstBook = {
 	img: 'https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL600_SR600,400_.jpg',
 }
 
-const author = 'James Clear'
-const title = 'Atomic Habits: the life-changing million-copy'
-const img =
-	'https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL600_SR600,400_.jpg'
+const secondBook = {
+	author: 'Julie Smith',
+	title: 'Why Has Nobody Told Me This Before?',
+	img: 'https://m.media-amazon.com/images/I/61vdz+IbbKL._SY466_.jpg',
+}
 
 const BookList = () => {
 	return (
 		<section className="booklist">
-			<Book author={author} title={title} img={img} />
-			<Book author={author} title={title} img={img} />
+			<Book
+				author={firstBook.author}
+				title={firstBook.title}
+				img={firstBook.img}
+			/>
+			<Book
+				author={secondBook.author}
+				title={secondBook.title}
+				img={secondBook.img}
+			/>
 			<Book />
 			<Book />
 		</section>
@@ -32,18 +41,6 @@ const Book = (props) => {
 			<h4>{props.author.toUpperCase()}</h4>
 		</article>
 	)
-}
-
-const firstBook = {
-	author: 'James Clear',
-	title: 'Atomic Habits',
-	img: 'https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL600_SR600,400_.jpg',
-}
-
-const secondBook = {
-	author: 'Julie Smith',
-	title: 'Why Has Nobody Told Me This Before?',
-	img: 'https://m.media-amazon.com/images/I/61vdz+IbbKL._SY466_.jpg',
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
