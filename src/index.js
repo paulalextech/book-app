1c1f214f7157ef4392c91ee0fc61744d61055f25
paul steane <psteane123@hotmail.comnpm install @mui/material @emotion/react @emotion/styled>
2024-01-04 10:15:47 +0000

    id assigned more books added

--- src/index.js
+++ src/index.js
@@ -1,84 +1,71 @@
-import React, { Children } from 'react'
-import ReactDOM from 'react-dom/client'
+import React, { Children } from 'react';
+import ReactDOM from 'react-dom/client';
 
-import './index.css'
-const firstBook = {
-	author: 'James Clear',
-	title: 'Atomic Habits: the life-changing million-copy',
-	img: 'https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL600_SR600,400_.jpg',
-}
+import './index.css';
+const books = [
+	{
+		author: 'James Clear',
+		title: 'Atomic Habits: the life-changing million-copy',
+		img: 'https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL600_SR600,400_.jpg',
+		id: 1,
+	},
 
-const secondBook = {
-	author: 'Julie Smith',
-	title: 'Why Has Nobody Told Me This Before?',
-	img: 'https://m.media-amazon.com/images/I/61vdz+IbbKL._SY466_.jpg',
-}
-
-const thirdBook = {
-	author: 'Richard Osman',
-	title: 'The Last Devil To Die: The Thursday Murder Club 4',
-	img: 'https://m.media-amazon.com/images/I/71Ja+kWW-DL._SY425_.jpg',
-}
+	{
+		author: 'Julie Smith',
+		title: 'Why Has Nobody Told Me This Before?',
+		img: 'https://m.media-amazon.com/images/I/61vdz+IbbKL._SY466_.jpg',
+		id: 2,
+	},
+	{
+		author: 'Richard Osman',
+		title: 'The Last Devil To Die: The Thursday Murder Club 4',
+		img: 'https://m.media-amazon.com/images/I/71Ja+kWW-DL._SY425_.jpg',
+		id: 3,
+	},
+	{
+		author: 'GT Karber',
+		title: 'Murdle:Solve 100 Devilishly Devious Murder Mystery Logic Puzzles ',
+		img: 'https://m.media-amazon.com/images/I/71ImfTaemnL._SY425_.jpg',
+		id: 4,
+	},
+	{
+		author: 'Sassy Quotes Press',
+		title:
+			'Fuck It 2024 Planner: Funny Weekly Organizer with Over 100 Sweary Affirmations and Badass Quotations',
+		img: 'https://m.media-amazon.com/images/I/61qxhMYr-CL._SY425_.jpg',
+		id: 5,
+	},
+	{
+		author: 'Rebecca Yarros',
+		title: 'Fourth Wing',
+		img: 'https://m.media-amazon.com/images/I/91xBRNN9AuL._SY425_.jpg',
+		id: 6,
+	},
+];
 
 const BookList = () => {
 	return (
 		<section className="booklist">
-			<Book
-				author={firstBook.author}
-				title={firstBook.title}
-				img={firstBook.img}
-			>
-				<p>
-					Transform your life with changes in behaviour, starting now. People
-					think that when you want to change your life, you need to think big.
-					But world-renowned habits expert James Clear has discovered another
-					way. He knows that real change comes from the compound effect of
-					hundreds of small decisions: doing two push-ups a day, waking up five
-					minutes early, or holding a single short phone call.
-				</p>
-				<button>Click Me</button>
-			</Book>
-
-			<Book
-				author={secondBook.author}
-				title={secondBook.title}
-				img={secondBook.img}
-			>
-				<p>
-					Give your mind the one thing it needs in 2024 with the book everyone
-					is STILL talking about, from clinical psychologist and TikTok
-					sensation Dr Julie Smith.
-				</p>
-				<button>Click Me</button>
-			</Book>
-			<Book
-				author={thirdBook.author}
-				title={thirdBook.title}
-				img={thirdBook.img}
-			>
-				<p>
-					The fourth novel in the record-breaking, million-copy bestselling
-					thursday murder club series. Shocking news reaches the Thursday Murder
-					Club.
-				</p>
-				<button>Click Me</button>
-			</Book>
+			{books.map((book) => {
+				const { img, title, author, id } = book;
+				return <Book img={img} title={title} author={author} key={id} />;
+			})}
 		</section>
-	)
-}
+	);
+};
 
-const Book = ({ img, title, author, children }) => {
+const Book = (props) => {
+	const { img, title, author } = props;
 	return (
 		<article className="book">
 			<img src={img} alt={title} />
 			<h2>{title}</h2>
 			<h4>{author.toUpperCase()}</h4>
-			{children}
 		</article>
-	)
-}
+	);
+};
 // above is preference for props. more dynamic.
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<BookList />)
+root.render(<BookList />);
