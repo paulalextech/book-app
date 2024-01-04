f2bcfcd8095d776dbbb56bc257d751c197c3de03
paul steane <psteane123@hotmail.comnpm install @mui/material @emotion/react @emotion/styled>
2024-01-04 11:24:50 +0000

    title components

--- src/index.js
+++ src/index.js
@@ -46,42 +46,24 @@ const books = [
 const BookList = () => {
 	return (
 		<section className="booklist">
-			<EventExamples />
 			{books.map((book) => {
-				return <Book book={book} key={book.id} />;
+				return <Book {...book} key={book.id} />;
 			})}
 		</section>
 	);
 };
 
-const EventExamples = () => {
-	return (
-		<section>
-			<form>
-				<h2>Search Form</h2>
-				<input
-					type="text"
-					name="product"
-					onChange={(e) => console.log(e.target.value)}
-					style={{ margin: '1rem 0' }}
-				/>
-				<button type="submit">submit</button>
-				<div>
-					<button onClick={() => console.log('click me')} type="button">
-						click me
-					</button>
-				</div>
-			</form>
-		</section>
-	);
-};
-
 const Book = (props) => {
-	const { img, title, author } = props.book;
+	const { img, title, author } = props;
+
+	const displayTitle = () => {
+		console.log(title);
+	};
 	return (
 		<article className="book">
 			<img src={img} alt={title} />
 			<h2>{title}</h2>
+			<button onClick={displayTitle}>display title</button>
 			<h4>{author.toUpperCase()}</h4>
 		</article>
 	);
