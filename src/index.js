0ed216b72a154cd732628f7d6241bffae1304465
paul steane <psteane123@hotmail.comnpm install @mui/material @emotion/react @emotion/styled>
2024-01-05 12:17:18 +0000

    changed font colors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Children } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
@@ -7,11 +7,15 @@ import Book from './Book';
 
 const BookList = () => {
 	return (
-		<section className="booklist">
-			{books.map((book, index) => {
-				return <Book {...book} key={book.id} number={index} />;
-			})}
-		</section>
+		<>
+			<h1>My Top 6 Space Books</h1>
+			<p>That I have actually read.</p>
+			<section className="booklist">
+				{books.map((book, index) => {
+					return <Book {...book} key={book.id} number={index} />;
+				})}
+			</section>
+		</>
 	);
 };
 
