e544fd65b7161c07fa04847ace9e33d4786554d7
paul steane <psteane123@hotmail.comnpm install @mui/material @emotion/react @emotion/styled>
2024-01-05 11:14:16 +0000

    import of src image folder

--- src/books.js
+++ src/books.js
@@ -1,42 +1,49 @@
+import img1 from './images/book-1.jpg';
+import img2 from './images/book-2.jpg';
+import img3 from './images/book-3.jpg';
+import img4 from './images/book-4.jpg';
+import img5 from './images/book-5.jpg';
+import img6 from './images/book-6.jpg';
+
 export const books = [
 	{
 		author: 'Gene Kranz',
 		title:
 			'Failure is Not an Option: Mission Control from Mercury to Apollo 13 and beyond',
-		img: 'https://m.media-amazon.com/images/I/91MSzpKOeiL._SY425_.jpg',
+		img: img1,
 		id: 1,
 	},
 
 	{
 		author: 'Annie-Sophie Baumann',
 		title: 'The Ultimate Book of Space: 3',
-		img: 'https://m.media-amazon.com/images/I/61HIaJNc+UL._SY425_.jpg',
+		img: img2,
 		id: 2,
 	},
 	{
 		author: 'Roger Wein',
 		title:
 			'Red Rover: Inside the Story of Robotic Space Exploration, from Genesis to the Mars Rover Curiosity',
-		img: 'https://m.media-amazon.com/images/I/61VJvQk+D2L._SY425_.jpg',
+		img: img3,
 		id: 3,
 	},
 	{
 		author: 'Neil Degrasse Tyson',
 		title: 'Cosmic Queries',
-		img: 'https://m.media-amazon.com/images/I/51r46e2cZBL._SY445_SX342_.jpg',
+		img: img4,
 		id: 4,
 	},
 	{
 		author: 'The Future of Humanity',
 		title:
 			'Terraforming Mars, Interstellar Travel, Immortality, and Our Destiny Beyond Earth',
-		img: 'https://m.media-amazon.com/images/I/81goZnqGdXL._SY425_.jpg',
+		img: img5,
 		id: 5,
 	},
 	{
 		author: 'Leonard David',
 		title: 'Mission to Mars',
-		img: 'https://m.media-amazon.com/images/I/41f44e2i92L._SY445_SX342_.jpg',
+		img: img6,
 		id: 6,
 	},
 ];
